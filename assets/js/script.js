b2e060fecc591f8f647819602050fcc6c4489790
LAgrl <lizethariasc14@gmail.com>
2021-05-16 14:22:46 -0700

    added current date in the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-var events = {}
 
-
-// Display today's day and date
+// display today's date in the header
 var today = moment().format('dddd, MMM Do YYYY');
 $("#currentDate").html(today);
+
+
+// color-code the time bock for past events
+
+// color-code the time block for present events 
+
+// color-code the time block for future events
